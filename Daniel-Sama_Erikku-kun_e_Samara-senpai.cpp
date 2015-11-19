26c68a860f94124072b07325b4abf0af68400ca0
danielmo1306 <danieloliveira1306@gmail.com>
2015-11-19 11:18:29 -0200

    Novo
    Inclui o processo! =)

--- Daniel-Sama_Erikku-kun_e_Samara-senpai.cpp
+++ Daniel-Sama_Erikku-kun_e_Samara-senpai.cpp
@@ -7,6 +7,13 @@ typedef struct Memoria{
 	int *vet;
 }memoria;
 
+typedef struct Processo{
+	int regis_base;
+	int regis_limite;
+	int id;
+	int tamanho;
+}processo;
+
 memoria* criarMemoria(int tamanho){
 	memoria *mem = new memoria;
 	mem->tamanho=tamanho;
@@ -20,8 +27,8 @@ void limparMemoria(memoria *mem){
 	}
 }
 
-void escreverMemoria(int conteudo, memoria *mem){
-	for(int i=0; i<mem->tamanho; i++){
+void escreverMemoria(int in,int fi,int conteudo, memoria *mem){
+	for(int i=in; i<fi; i++){
 		mem->vet[i]=conteudo;
 	}
 }
@@ -30,6 +37,34 @@ void liberarMemoria(memoria *mem){
 	delete mem->vet;
 	delete mem;
 }
+
+//Até aqui é o da memória
+
+processo* criarProcesso(int regis_limite, int regis_base, int id, int tamanho){
+	processo *pro = new processo;
+	pro->regis_limite = regis_limite;
+	pro->regis_base = regis_base;
+	pro->tamanho = tamanho;
+	return pro;
+}
+
+void liberarProcesso(processo *pro){
+	delete pro;
+}
+
+void escreverProcesso (memoria *mem, processo *pro){
+	escreverMemoria(pro->regis_base,pro->regis_limite,pro->id,mem);
+}
+
+void guardarProcesso (memoria *mem, processo *pro,int cont){
+	escreverMemoria(pro->regis_base,pro->regis_limite,cont,mem);
+}
+
+void retiraProcesso (memoria *mem, processo *pro){
+	escreverMemoria(pro->regis_base,pro->regis_limite,0,mem);
+}
+
+//Até aqui é o do processo
 
 int main(){
 	return 0;
